1804cf2ebd5e27a781af65e135d550be38122024
Monica Powell <mmp2181@columbia.edu>
2017-04-05 22:19:38 -0400

    update

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def hello():
 
 def getTweets():
     word_to_search = format(request.form['word_to_search'])
-    response = twitter.search(q=word_to_search + " -filter:retweets", result_type="recent", count=10)
+    response = twitter.search(q=word_to_search + " -filter:retweets AND -filter:replies", result_type="recent", count=10)
     try:
         tweet_list = []
         id_num = 0
@@ -31,7 +31,7 @@ def getTweets():
             tweet_list.append(tweet['text'])
 
 
-            ctt_text_area = '<a href="https://twitter.com/intent/retweet?tweet_id=' + tweet["id_str"] + '">Click to RT</a> <button onclick="toggleLink(id)" id="button-'+ str(id_num)+'"type="button">Get Click to RT Link</button> <div style="display: none;"><textarea rows="4" cols="50" id="button-'+ str(id_num)+'">' + "https://twitter.com/intent/retweet?tweet_id=" + tweet["id_str"] + '</textarea> <p>Share this link. When clicked it will prompt people to RT the specified tweet.</p></div>'
+            ctt_text_area = '<div class="sharebuttons"><a href="https://twitter.com/intent/retweet?tweet_id=' + tweet["id_str"] + '">Click to RT</a> <button onclick="toggleLink(id)" id="button-'+ str(id_num)+'"type="button">Get Click to RT Link</button> <span style="display: none;"><textarea rows="4" cols="50" id="button-'+ str(id_num)+'">' + "https://twitter.com/intent/retweet?tweet_id=" + tweet["id_str"] + '</textarea> <p>Share this link. When clicked it will direct people Twitter and confirm that they would like RT the specified tweet.</p></span></div>'
             tweet_list.append(ctt_text_area)
             id_num = id_num + 1
 
